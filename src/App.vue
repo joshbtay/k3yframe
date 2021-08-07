c77fce1b072ebce40650195260d3c9f9aa8c0ba7
Josh T <joshbtay@gmail.com>
2021-08-06 22:35:21 -0600

    first commit, added basic functionality

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/"><h1>K<strong>3</strong>y Frame</h1></router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+*{
+  background-color: #222;
+}
+#app {
+  font-family: "Roboto Mono", monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+}
+h1{
+	font-weight: 200;
+}
+textarea{
+	background-color: #333;
+	color: white;
+	width: 70%;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  color: #cfcf54;
+}
+
+#nav a.router-link-exact-active {
+  color: #cfcf54;
+  font-weight: 300;
+  text-decoration: none;
+}
+button{
+	margin: 20px;
+	padding: 10px;
+	border-radius: 5px;
+	border: none;
+	color: white;
+	background-color: #333;
+	transition: all .2s;
+}
+button:hover{
+	background-color: #444;
+}
+button:active{
+	background-color: #555;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="home">
+  <button @click="english = !english">switch languages</button>
+  <br>
+  <textarea :placeholder="message" v-model="text" rows="10"></textarea>
+  <p id="output">{{ out }}</p>
+  <button class="copy" @click="copytext">copy</button>
+  <button v-if="!english && lower" @click="lower = !lower">UPPERCASE</button>
+  <button v-if="!english && !lower" @click="lower = !lower">lowercase</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  mounted() {
+	for (var key in this.translate){
+		this.inverted[this.translate[key]] = key;
+	}
+	console.log(this.inverted)
+  },
+  data() {
+	return {
+		english: true,
+		lower: true,
+		text: '',
+		translate: {
+			'A': '|',
+			'B': '┤',
+			'C': '╭',
+			'D': '┘',
+			'E': '3',
+			'F': '╕',
+			'G': '╗',
+			'H': '╫',
+			'I': '┇',
+			'J': '╛',
+			'K': '╮',
+			'L': '╪',
+			'M': '┬',
+			'N': '╖',
+			'O': '┼',
+			'P': '╝',
+			'Q': '╜',
+			'R': '╡',
+			'S': '┅',
+			'T': '╤',
+			'U': '╧',
+			'V': '┴',
+			'W': '╩',
+			'X': '╬',
+			'Y': '╣',
+			'Z': '╨',
+			' ': '_',
+			'1': '⏈',
+			'2': '⏆',
+			'3': '⏇',
+			'4': '⏂',
+			'5': '⊕',
+			'6': '⏁',
+			'7': '⏅',
+			'8': '⏃',
+			'9': '⏄',
+			'0': '⍏',
+		},
+		inverted: {},
+	}
+  },
+  computed: {
+	out() {
+		return this.output();
+	},
+	message(){
+		if(this.english)
+		return "ent3r text t0 transl@t3";
+		else
+		return "3╖╤⏇╡_╤3╬╤_╤⍏_╤╡|╖┅╪@╤3";
+	}
+  },
+  methods: {
+	output() {
+		let out = ''
+		let text = this.text.toUpperCase()
+		if(this.english){
+		for(var i = 0; i < text.length; i++){
+			if(!(text.charAt(i) in this.translate)){
+				out += text.charAt(i)
+			}
+			else{
+				out += this.translate[text.charAt(i)]
+			}
+		}
+		}else{
+		for(var j = 0; j < text.length; j++){
+			if(!(text.charAt(j) in this.inverted)){
+				out += text.charAt(j)
+			}
+			else{
+				out += this.inverted[text.charAt(j)]
+			}
+			
+		}
+		if (this.lower){
+			out = out.toLowerCase()
+		}
+		}
+		return out;
+	},
+	copytext(){
+		let text = this.output()
+		navigator.clipboard.writeText(text);
+	},
+  }
+
+}
+</script>
